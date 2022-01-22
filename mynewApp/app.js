6f91018d9627e819f8570d52cf27bdc32002a139
Bharath B <bharath.b@intimetec.com>
2022-01-22 22:34:18 +0530

    added custom middleware

--- mynewApp/app.js
+++ mynewApp/app.js
@@ -19,7 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+const customMiddleWare = (req,res,next)=>{
+  console.log("some logs")
+  next()
+}
+app.use('/',customMiddleWare, indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
