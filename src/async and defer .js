cfdaf16826221b7c87a6b9ed30f4305092d3ac19
Bharath B <bharath.b@intimetec.com>
2022-10-19 17:43:44 +0530

    async defer

---
+++ src/async and defer .js
@@ -0,0 +1,19 @@
+normal script
+html pasring                                html parsing
+script found  script executed
+
+
+async
+html pasring                         html parsing
+script found  script executed
+
+
+
+defer
+html pasringhtml parsing
+script found            script executed
+
+
+
+
+if one script is depend on another then don't use async, use defer
