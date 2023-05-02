b0f550477ba522bca49a7634e641ca7026c648e6
Bharath B <bharath.b@intimetec.com>
2023-05-02 20:01:02 +0530

    added docs for worker nodejs

---
+++ src/worker.js
@@ -0,0 +1,2 @@
+https://livecodestream.dev/post/how-to-work-with-worker-threads-in-nodejs/
+https://www.digitalocean.com/community/tutorials/node-js-architecture-single-threaded-event-loop#node-js-architecture
