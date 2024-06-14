996c19b03967f143947d6a3c5516df0bde49aa61
Bharath B <98091989+bharathbdev@users.noreply.github.com>
2024-06-14 17:17:49 +0530

    Update singlethread.js

--- src/singlethread.js
+++ src/singlethread.js
@@ -1,1 +1,2 @@
 https://www.digitalocean.com/community/tutorials/node-js-architecture-single-threaded-event-loop
+https://dev.to/trunghieu99tt/how-does-nodejs-handle-thousands-of-requests-while-its-single-thread-dcp
