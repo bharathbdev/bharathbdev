10c0ef67308937a916b56940fb3a6c134c371a5a
Bharath B <98091989+bharathbdev@users.noreply.github.com>
2024-06-25 17:44:04 +0530

    restructure

--- src/interview/basic questions.js
+++ src/interview/basic questions.js
@@ -554,3 +554,35 @@ console.log(res)
 
 let arr =[1,4,3,0,9,0]
 let output = [1,4,3,0,0,9,0,0]
+
+
+
+function getMinAndMaxTime(maxRetries, passwords, correctPassword) {
+    let retries = maxRetries;
+    let minTime = 0;
+    let maxTime = 0;
+    let found = false;
+    for (let i = 0; i < passwords.length; i++) {
+        retries -= 1;
+        if (!found)
+            minTime += 1;
+
+        maxTime += 1;
+        if (correctPassword === passwords[i])
+            found = true;
+
+        if (correctPassword.length < passwords[i].length)
+            break;
+
+        if (retries <= 0) {
+            retries = maxRetries;
+            if (!found) {
+                minTime += 5;
+            }
+            maxTime += 5;
+        }
+    }
+    return [minTime, maxTime];
+}
+
+console.log(getMinAndMaxTime(2, ["awd", "aa1", "bbb", "ccc", "ddd", "qqq", "qawdw"], "aa1"));
